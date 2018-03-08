e3ba02fff98e99bbefc3632a8a92efbc817c4e3e
ybdesire <ybdesire@gmail.com>
2018-03-08 21:58:04 +0800

    completed sys layout and two activity switching.

--- AndroidCpuMonitor/app/src/main/java/cpumonitor/ybdesire/com/androidcpumonitor/SysMonitorActivity.java
+++ AndroidCpuMonitor/app/src/main/java/cpumonitor/ybdesire/com/androidcpumonitor/SysMonitorActivity.java
@@ -1,28 +1,32 @@
 package cpumonitor.ybdesire.com.androidcpumonitor;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SysMonitorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sys_monitor);
 
 
-        Button btn=(Button)findViewById(R.id.btn_sys);//find button by id(defined at activity_main.xml)
+        Button btn=(Button)findViewById(R.id.btn_home);//find button by id(defined at activity_main.xml)
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intnt = new Intent(MainActivity.this, SysMonitorActivity.class);
+                Intent intnt = new Intent(SysMonitorActivity.this, MainActivity.class);
                 startActivity(intnt);
             }
         });
 
 
     }
+
 }
