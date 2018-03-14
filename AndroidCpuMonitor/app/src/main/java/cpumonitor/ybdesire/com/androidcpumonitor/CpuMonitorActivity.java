20f91e8a68ff04574e6fb1b4562619bd1effd53e
ybdesire <ybdesire@gmail.com>
2018-03-14 22:04:29 +0800

    repeat textView update task with time delay.

--- AndroidCpuMonitor/app/src/main/java/cpumonitor/ybdesire/com/androidcpumonitor/CpuMonitorActivity.java
+++ AndroidCpuMonitor/app/src/main/java/cpumonitor/ybdesire/com/androidcpumonitor/CpuMonitorActivity.java
@@ -1,6 +1,7 @@
 package cpumonitor.ybdesire.com.androidcpumonitor;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,32 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class CpuMonitorActivity extends AppCompatActivity {
+
+    private int mInterval = 1000; // 1 seconds by default, can be changed later
+    private Handler mHandler;
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                double cpuUsage = 100 * DeviceUtil.getCpuUsage();
+                // text view details
+                String str = "\n\n";
+                str += "CPU : " + cpuUsage + "\n\n";
+                TextView txt = (TextView) findViewById(R.id.txtDetails);//find output label by id
+                txt.setText(str);
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler.postDelayed(mStatusChecker, mInterval);//将指定Runnable（包装成PostMessage）加入到MessageQueue中，然后Looper不断从MessageQueue中读取Message进行处理
+            }
+        }
+    };
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +51,8 @@ public class CpuMonitorActivity extends AppCompatActivity {
             }
         });
 
-        double cpuUsage = 100*DeviceUtil.getCpuUsage();
-        // text view details
-        String str = "\n\n";
-
-        str += "CPU : " + cpuUsage + "\n\n";
-
-        TextView txt=(TextView)findViewById(R.id.txtDetails);//find output label by id
-        txt.setText(str);
-
+        // get cpu usage and update by 1s timer
+        mHandler = new Handler();
+        startRepeatingTask();
     }
 }
